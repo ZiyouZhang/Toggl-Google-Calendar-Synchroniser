6bb57f7dce05425111bb6f7fcbf99aa0f0de886d
Ziyou Zhang <ziyouzhangofficial@gmail.com>
2022-04-10 21:39:27 +0000

    Added more time entry obtaining implementations.

--- toggl/toggl.go
+++ toggl/toggl.go
@@ -9,23 +9,34 @@ import (
 )
 
 const (
-	URL       = "https://api.track.toggl.com/reports/api/v2/details"
-	layoutISO = "2006-01-02"
+	URL          = "https://api.track.toggl.com/reports/api/v2/details"
+	layoutISO    = "2006-01-02"
+	authPassword = "api_token"
+	userAgent    = "application"
 )
 
 var (
-	workspace_id = os.Getenv("TOGGL_WORKSPACE_ID")
+	workspaceId = os.Getenv("TOGGL_WORKSPACE_ID")
+	apiKey      = os.Getenv("TOGGL_API_TOKEN")
 )
 
 func TimeEntries(token string) {
 	today := time.Now().Format(layoutISO)
-	yearago := time.Now().AddDate(-1, 0, 0).Format(layoutISO)
-	url := fmt.Sprintf("%v?workspace_id=%v&since=%v&until=%v&user_agent=api_integration", URL, workspace_id, yearago, today)
-	resp, err := http.Get(url)
+	yearAgo := time.Now().AddDate(-1, 0, 0).Format(layoutISO)
+	url := fmt.Sprintf("%v?workspace_id=%v&since=%v&until=%v&user_agent=%v", URL, workspaceId, yearAgo, today, userAgent)
+	fmt.Println(url)
+	c := http.Client{Timeout: time.Duration(10) * time.Second}
+	req, err := http.NewRequest("Get", url, nil)
+	if err != nil {
+		log.Printf("Error when generating request: %v", err)
+	}
+	fmt.Println(apiKey)
+	req.SetBasicAuth(apiKey, authPassword)
+	resp, err := c.Do(req)
 	if err != nil {
 		log.Printf("Error when querying url: %v", err)
 	}
-	fmt.Print(resp)
+	fmt.Print(resp.Status)
 }
 
 func Bar() bool {
