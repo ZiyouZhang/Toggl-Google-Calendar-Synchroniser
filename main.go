27698c8fc86cba467d19a9b92f29b2a001e7187c
Ziyou Zhang <ziyouzhangofficial@gmail.com>
2023-02-25 18:29:00 +0000

    Updated implementations.

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+
 	"syncroniser/gcal"
 	"syncroniser/toggl"
 )
 
 func main() {
-	gcal.Foo()
+	ctx := context.Background()
+
+	srv := gcal.GetService(ctx)
+
+	cals, err := srv.CalendarList.List().Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve calendars: %v", err)
+	}
+	fmt.Println("Calendars:")
+	for _, item := range cals.Items {
+		fmt.Printf("%v\n", item.Summary)
+	}
+	
 	toggl.TimeEntries()
 }
